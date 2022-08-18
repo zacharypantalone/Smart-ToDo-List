7708f6dc947874fd7edfa902f5fbf0d3c2ff0e9b
Zachary Pantalone <zachary.pantalone@gmail.com>
2022-08-18 16:43:24 -0400

    changed email to username in index.ejs

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ app.post('/main', (req, res) => {
   // req.session.user_id = req.params.id;
 
   // or using plain-text cookies
-  res.cookie('user_id', req.params.id);
+  res.cookie(req.params.id);
 
   // send the user somewhere
   res.redirect('/main');
