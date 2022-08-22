ad8c2f925b004fb0a1b73cc36fadb9d8899e4b67
Zachary Pantalone <zachary.pantalone@gmail.com>
2022-08-22 19:21:47 -0400

    changes

--- db/seeds/02_categories.sql
+++ db/seeds/02_categories.sql
@@ -1,4 +1,4 @@
 INSERT INTO categories (id, category_type) VALUES (1, 'Movie');
 INSERT INTO categories (id, category_type) VALUES (2, 'Restaurant');
-INSERT INTO categories (id, category_type) VALUES (1, 'Books');
-INSERT INTO categories (id, category_type) VALUES (2, 'Products');
+INSERT INTO categories (id, category_type) VALUES (3, 'Books');
+INSERT INTO categories (id, category_type) VALUES (4, 'Products');
