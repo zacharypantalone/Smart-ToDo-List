4792a7e9472207356c9bc47db510d95fce55bc0c
Zachary Pantalone <zachary.pantalone@gmail.com>
2022-08-22 19:11:44 -0400

    updated both 02_categories seeds and schema

--- db/schema/02_categories.sql
+++ db/schema/02_categories.sql
@@ -1,5 +1,5 @@
 DROP TABLE IF EXISTS categories CASCADE;
 CREATE TABLE categories (
-  id SERIAL PRIMARY KEY NOT NULL,
-  category_type VARCHAR(255)
+  id INTEGER,
+  category_type VARCHAR(50)
 );
--- db/seeds/02_categories.sql
+++ db/seeds/02_categories.sql
@@ -1,2 +1,4 @@
-INSERT INTO categories (category_type) VALUES ('movie');
-INSERT INTO categories (category_type) VALUES ('book');
+INSERT INTO categories (id, category_type) VALUES (1, 'Movie');
+INSERT INTO categories (id, category_type) VALUES (2, 'Restaurant');
+INSERT INTO categories (id, category_type) VALUES (1, 'Books');
+INSERT INTO categories (id, category_type) VALUES (2, 'Products');
